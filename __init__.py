4a3e0d2167ba360393613ed84c90aaeb671cef73
Pengfei.Yu <border1px@163.com>
2024-07-20 10:51:20 +0800

    first

---
+++ __init__.py
@@ -0,0 +1,9 @@
+from .image_selector_node import ImageSelector
+
+NODE_CLASS_MAPPINGS = {
+    "Image Selector" : ImageSelector,
+}
+
+WEB_DIRECTORY = "./js"
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'WEB_DIRECTORY']
